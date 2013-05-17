848502e999af08f9467d8190e897d4a381d9576f
Atul <amkcat9@gmail.com>
2013-05-17 18:07:32 +0530

    forum configuration

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbhost = "127.0.0.1";
+$dbuser = "atul";
+$dbpassword = "mypasswd";
+$dbdatabase = "forum";
+
+// Add the name of the forums below
+$config_forumsname = "CineForums";
+
+// Add your name below
+$config_admin = "Atul Karmarkar";
+$config_adminemail = "atul.karmarkar AT gmail DOT com";
+
+// Add the location of your forums below
+$config_basedir = "http://127.0.0.1/sites/forum/";
+
+?>
+
